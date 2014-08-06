99076cf0366e913ae53f41b957082f28b07ff146
Phu Le <lqp.2792@gmail.com>
2014-08-06 19:58:57 +0700

    Sửa lại Cấu trúc project. Bắt đầu viết crawl Voice bằng Selenium

---
+++ src/main/java/phu/quang/le/Selenium/VoiceCrawler.java
@@ -0,0 +1,22 @@
+package phu.quang.le.Selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+public class VoiceCrawler {
+
+	public static void main (String[] args) throws Exception {
+		WebDriver driver = new HtmlUnitDriver ();
+		driver.get ("http://text-to-speech.imtranslator.net/");
+		driver.switchTo ().frame (driver.findElement (By.id ("TTSText")));
+		WebElement element = driver.findElement (By.id ("text"));
+		element.sendKeys ("情報");
+		driver.switchTo ().defaultContent ();
+		driver.findElement (By.id ("TTSLangs")).click ();
+		driver.findElement (By.xpath ("//option[value='ja']")).click ();;
+		driver.findElement (By.id ("ttsgo")).click ();
+		System.out.println(driver.getCurrentUrl ());
+	}
+}
--- src/main/java/phu/quang/le/Utils/ParseData.java
+++ src/main/java/phu/quang/le/Utils/ParseData.java
@@ -1,13 +1,9 @@
 package phu.quang.le.Utils;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class ParseData {
